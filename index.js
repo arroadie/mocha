07e87ca50ffd05c31923c6667d2151c57f737b2c
satanas <wil.alejandro@gmail.com>
2016-02-22 13:56:55 -0300

    Serving web app with koa

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var koa
+var fs = require('fs');
+var koa = require('koa');
+var router = require('koa-router')();
+var serve = require('koa-serve');
+
 var server = require('socket.io')(3000);
 
 var thread = {
@@ -12,6 +16,8 @@ var thread = {
   timestamp: 1456154319
 };
 
+var users = {};
+
 server.on('connection', function(socket) {
   console.log(`Connected client: ${socket.id}`);
 
@@ -21,5 +27,25 @@ server.on('connection', function(socket) {
   });
 });
 
-console.log('Listening on port 3000');
+
+// Web app
+var app = koa();
+
+app.use(serve('public'));
+app.use(function* (next) {
+  const start = new Date();
+  yield next;
+  const end = new Date();
+
+  console.log(`[WebApp][${start.toISOString()}] :: ${this.method} ${this.url} processed in ${String(end-start)} ms`);
+});
+
+router.get('/', function(next) {
+  this.body = fs.readFileSync('index.html').toString();
+});
+app.use(router.routes());
+
+app.listen(8001);
+console.log('WebApp listening on port 8001');
+console.log('Server listening on port 3000');
 
