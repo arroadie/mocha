398ea62e230ab2c10c151ac263252f87dfa0ec05
satanas <wil.alejandro@gmail.com>
2016-02-22 12:42:54 -0300

    Added koa static and message broadcast

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-//var app = require('http').createServer(handler);
+var koa
 var server = require('socket.io')(3000);
 
+var thread = {
+  id: 'test',
+  parent_id: null,
+  type: null,
+  has_children: true,
+  user_id: 'francis',
+  user_name: 'Francis',
+  message: '',
+  timestamp: 1456154319
+};
+
 server.on('connection', function(socket) {
-  console.log('soksokdoaksd', socket.id);
+  console.log(`Connected client: ${socket.id}`);
 
   socket.on('message', function(data) {
     console.log(`Message ${socket.id}: ${data}`);
+    server.emit('message', data);
   });
 });
 
