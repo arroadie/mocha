2940e9d886598884e0ad72dd64081699e3410760
Antonio Bastardo <abastardosalas@groupon.com>
2016-02-23 11:43:38 -0300

    added escape to  HTML texts

--- public/js/app.js
+++ public/js/app.js
@@ -107,7 +107,7 @@ function sendMessage() {
 function printMessage(data) {
   var date = new Date(data.timestamp );
   var classMessage = (data.username === user()) ? 'leftuser' : 'left';
-  $("#messages ul").append(`<li class=${classMessage}><span><p class="message-header"><b class="username">${data.user_name}</b> <b class="timestamp">${date.toGMTString()}</b></p><p class="message">${data.message}</p></span></li>`);
+  $("#messages ul").append(`<li class=${classMessage}><span><p class="message-header"><b class="username">${data.user_name}</b> <b class="timestamp">${date.toGMTString()}</b></p><p class="message">${escapeHtml(data.message)}</p></span></li>`);
   updateChatScroll();
 }
 
@@ -115,3 +115,13 @@ function resizeWindow() {
   var height = $(window).innerHeight() - 130;
   $('#chat-box').css('height', height + 'px');
 }
+
+
+function escapeHtml(unsafe) {
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+ }
